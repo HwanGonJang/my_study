2f9d92db132d350cd512a60e500275ff2acdb065
Jin <thinkjin99@gmail.com>
2023-09-20 22:11:37 +0900

    cannonical mode

--- multy/multy_client.c
+++ multy/multy_client.c
@@ -5,7 +5,7 @@
 #include <arpa/inet.h>
 #include <fcntl.h>
 #include <pthread.h>
-
+#include <termios.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/select.h>
@@ -13,6 +13,7 @@
 #define PORT 8000
 #define MAX_MESSAGE_LEN 128
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+struct termios initial_settings, new_settings;
 
 struct my_socket
 {
@@ -21,6 +22,11 @@ struct my_socket
     char buffer[MAX_MESSAGE_LEN];
     char write_buffer[MAX_MESSAGE_LEN];
 };
+
+void reset_terminal()
+{
+    tcsetattr(STDIN_FILENO, TCSANOW, &initial_settings);
+}
 
 void *read_with_timeout(void *arg)
 {
@@ -33,11 +39,23 @@ void *read_with_timeout(void *arg)
     struct my_socket *socket = (struct my_socket *)arg;
     timeout.tv_sec = 1;
     timeout.tv_usec = 0;
+    memset(socket->buffer, 0, MAX_MESSAGE_LEN);
+    memset(socket->write_buffer, 0, MAX_MESSAGE_LEN);
+
+    int flags = fcntl(STDIN_FILENO, F_GETFL);
+    fcntl(STDIN_FILENO, F_SETFL, flags | O_NONBLOCK);
+    int index = 0;
+    char unicode[20];
+
     while (1)
     {
         FD_ZERO(&readFds);
+        FD_SET(STDIN_FILENO, &readFds);
         FD_SET(socket->socket, &readFds);
         select(socket->socket + 1, &readFds, NULL, NULL, &timeout);
+        printf("\r\033[K");
+        printf("\r>> %s", socket->write_buffer);
+        fflush(stdout);
 
         if (FD_ISSET(socket->socket, &readFds))
         {
@@ -45,7 +63,6 @@ void *read_with_timeout(void *arg)
             if (rx_len > 0)
             {
                 pthread_mutex_lock(&mutex);
-                printf("\033[F");
                 printf("\n%s\n", socket->buffer);
                 pthread_mutex_unlock(&mutex);
                 memset(socket->buffer, 0, MAX_MESSAGE_LEN);
@@ -56,55 +73,43 @@ void *read_with_timeout(void *arg)
                 exit(0);
             }
         }
+        if (FD_ISSET(STDIN_FILENO, &readFds))
+        {
+            pthread_mutex_lock(&mutex);
+
+            rx_len = read(STDERR_FILENO, unicode, 5);
+            if ((c != '\n'))
+                printf("key got: %s\n", c);
+            // socket->write_buffer[index++] = c;
+            // else
+            // {
+            //     if (strcmp(socket->write_buffer, "exit") == 0)
+            //         break;
+            //     write(socket->socket, socket->write_buffer, index);
+            //     memset(socket->write_buffer, 0, MAX_MESSAGE_LEN);
+            //     index = 0;
+            // }
+            pthread_mutex_unlock(&mutex);
+            // }
+        }
     }
     return NULL;
-}
-
-char *input(char *buffer)
-{
-    int i = 0;
-    while (1)
-    {
-        printf("\r\033[K");
-        printf(">> %s", buffer);
-        int c = getchar();         // 문자 하나를 읽음
-        if (c == '\n' || c == EOF) // 줄바꿈 등의 이스케이프 문자를 넣어야 서버에서 바이트의 종료시점을 파악한다.
-        {                          // '\n' 또는 EOF(End Of File)를 만나면 입력 종료
-            break;
-        }
-        buffer[i++] = c; // 입력 배열에 문자 추가
-    }
-    return buffer;
-}
-
-void *write_message(void *arg)
-{
-    struct my_socket *socket = (struct my_socket *)arg;
-    char *write_buffer = socket->write_buffer;
-    memset(write_buffer, 0, MAX_MESSAGE_LEN);
-
-    while (1)
-    {
-        // printf("\r\033[K");
-        input(write_buffer);
-        if ((strncmp(write_buffer, "exit", 4)) == 0)
-        {
-            printf("\nClient exit...");
-            return NULL;
-        }
-        write(socket->socket, write_buffer, strlen(write_buffer));
-        memset(write_buffer, 0, MAX_MESSAGE_LEN);
-    }
 }
 
 int main()
 {
     int client_socket;
-    // char write_buffer[MAX_MESSAGE_LEN];
     // 클라이언트 소켓 생성
     client_socket = socket(AF_INET, SOCK_STREAM, 0);
     struct sockaddr_in server_address;
     pthread_t read_msg, write_msg, render_msg;
+
+    tcgetattr(STDIN_FILENO, &initial_settings);
+    new_settings = initial_settings;
+    new_settings.c_lflag &= ~ICANON; // 정규 모드 비활성화
+    new_settings.c_lflag &= ~ECHO;   // 에코 비활성화
+    tcsetattr(STDIN_FILENO, TCSANOW, &new_settings);
+    atexit(reset_terminal);
 
     if (client_socket == -1)
     {
@@ -133,17 +138,16 @@ int main()
 
     // 닉네임 입력
     printf("Input your name\n");
-    char name[20];
-    input(name);
+    printf(">> ");
+    char name[20] = "jmj";
     write(socket.socket, name, strlen(name));
-
     pthread_create(&read_msg, NULL, read_with_timeout, &socket);
-    pthread_create(&write_msg, NULL, write_message, &socket);
-
-    pthread_join(write_msg, NULL);
-
-    // 클라이언트 소켓 닫기
-    pthread_cancel(read_msg);
-    close(client_socket);
-    return 0;
+    pthread_join(read_msg, NULL);
+    return EXIT_SUCCESS;
 }
+
+// 클라이언트 소켓 닫기
+// pthread_cancel(read_msg);
+// close(client_socket);
+// return 0;
+// }
